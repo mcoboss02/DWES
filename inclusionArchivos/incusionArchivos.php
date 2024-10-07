036425d4c92d5a13f8466569e204dcbaab0ea672
mcoboss02 <125301244+mcoboss02@users.noreply.github.com>
2024-10-07 08:55:21 +0200

    Create incusionArchivos.php

---
+++ inclusionArchivos/incusionArchivos.php
@@ -0,0 +1,9 @@
+<?php
+// Autor: Manuel Cobos Solís
+// Fecha: 06/10/2024
+// Uso de include y require
+
+include 'header.php'; // Incluye un archivo externo
+require 'config.php'; // Incluye un archivo obligatorio
+echo "Este es el contenido principal de la página.<br/>";
+?>
